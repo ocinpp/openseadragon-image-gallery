8230c3ec9699b89129a4b09dde9a6ca1e5ca280b
ocinpp <ocin25@gmail.com>
2025-03-02 03:44:09 +0800

    Move data to separate file

---
+++ data.js
@@ -0,0 +1,70 @@
+// Sample images from Unsplash
+const images = [
+  {
+    url: "https://images.unsplash.com/photo-1682687982501-1e58ab814714",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682687982501-1e58ab814714?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682687218147-9806132dc697",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682687218147-9806132dc697?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682687220063-4742bd7fd538",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682687220063-4742bd7fd538?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682695796954-bad0d0f59ff1",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682695796954-bad0d0f59ff1?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682685797208-c741d58c2eff",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682685797208-c741d58c2eff?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682686581030-7fa4ea2b96c3",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682686581030-7fa4ea2b96c3?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1740680209886-c461a9c692f3",
+    thumbnail:
+      "https://images.unsplash.com/photo-1740680209886-c461a9c692f3?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682686578842-00ba49b0a71a",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1682686580186-b55d2a91053c",
+    thumbnail:
+      "https://images.unsplash.com/photo-1682686580186-b55d2a91053c?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1485038101637-2d4833df1b35",
+    thumbnail:
+      "https://images.unsplash.com/photo-1485038101637-2d4833df1b35?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1618915340727-c768b99dc6bb",
+    thumbnail:
+      "https://images.unsplash.com/photo-1618915340727-c768b99dc6bb?h=200&fit=crop",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1512138664757-360e0aad5132",
+    thumbnail:
+      "https://images.unsplash.com/photo-1512138664757-360e0aad5132?h=200&fit=crop",
+  },
+];
+
+export { images };
